f9562906792f132e6d75e247ee62d5456cc3e4e4
Anjel <angelica.gomez@unwritten.studio>
2026-06-12 08:06:59 +0200

    Add: Kaspar Hauser Quest game - integrated as route /kaspar

---
+++ app/kaspar/page.tsx
@@ -0,0 +1,14 @@
+export default function KasparPage() {
+  return (
+    <iframe
+      src="/kaspar-game/index.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        display: 'block'
+      }}
+      title="Kaspar Hauser Quest"
+    />
+  );
+}
